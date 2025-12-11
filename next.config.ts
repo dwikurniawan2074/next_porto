c07aa0afe56bb88bdc84692f9d1804a8ab10b149
Dwi Kurniawan <dwi.kurniawan@pgnmas.co.id>
2025-12-11 15:21:04 +0700

    update path again

--- next.config.ts
+++ next.config.ts
@@ -4,17 +4,12 @@ const basePath = process.env.NODE_ENV === "production" ? "/new-portfolio" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath,
-  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
-  },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
   },
 };
 
